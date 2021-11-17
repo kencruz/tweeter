059652a5e6670055ba55d205f8d2fa8320e4f55e
Ken Cruz <ken@kencruz.ca>
2021-11-17 17:48:49 -0500

    implemented ajax load tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(() => {
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
-
   const renderTweets = function (tweets) {
     // loops through tweets
     for (const tweet of tweets) {
@@ -65,7 +40,12 @@ $(document).ready(() => {
               </article>
       `);
   };
-  renderTweets(data);
+
+  const loadTweets = () => {
+    return $.ajax("/tweets", { method: "GET" }).then(function (tweets) {
+      renderTweets(tweets);
+    });
+  };
 
   $("form").submit(function () {
     event.preventDefault();
@@ -77,7 +57,10 @@ $(document).ready(() => {
       data: tweetContent,
       success: function (done) {
         console.log("tweet sent");
+        loadTweets();
       },
     });
   });
+
+  loadTweets();
 });
