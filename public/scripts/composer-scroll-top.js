4924146a0096e720e63332bd7c508aae24f7bf73
Ken Cruz <ken@kencruz.ca>
2021-11-18 18:23:49 -0500

    added sticky nav bar

--- public/scripts/composer-scroll-top.js
+++ public/scripts/composer-scroll-top.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
   $(window).on('scroll', function() {
     const height = $(this).scrollTop();
 
+    if (height > 399) {
+      $('nav').addClass('nav-anchor');
+    } else {
+      $('nav').removeClass('nav-anchor');
+    }
+
     if (height > 500) {
       $('#scroll-up').fadeIn();
     } else {
